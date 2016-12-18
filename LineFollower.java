1317a7777f1708ce9baa618717b0790abdd14806
ArthurvanStrien <arthurvanstrien@gmail.com>
2016-12-18 17:31:59 +0100

    Added the LineFollower classed that when called can make the BoeBot follow a line.

---
+++ LineFollower.java
@@ -0,0 +1,51 @@
+import TI.*;
+/**
+ * Write a description of class LineFollower here.
+ * 
+ * @author  (Arthur van Strien) 
+ * @version (1)
+ * @date    ()
+ */
+public class LineFollower
+{
+    Drive drive;
+    LineDetectors lineDetectors;
+    private int left;
+    private int right;
+    
+    public LineFollower()
+    {
+        drive = new Drive();
+        lineDetectors = new LineDetectors();
+        left = 0;
+        right = 0;
+    }
+    
+    public void followLine()
+    {   
+        if(lineDetectors.leftBlack() == true && lineDetectors.rightBlack() == false)
+        {
+            left = 0;
+            right = right + 2;
+            drive.right(right);
+        }
+        else if(lineDetectors.rightBlack() == false && lineDetectors.rightBlack() == true)
+        {
+            right = 0;
+            left = left + 2;
+            drive.left(left);
+        }
+        else if(lineDetectors.rightBlack() == false && lineDetectors.leftBlack() == false && lineDetectors.middleBlack() == true)
+        {
+            drive.forward();
+        }
+        else if(lineDetectors.rightBlack() == true && lineDetectors.leftBlack() == true && lineDetectors.middleBlack() == true)
+        {
+            //Crossing
+        }
+        else
+        {
+            drive.stop();
+        }
+    }
+}
