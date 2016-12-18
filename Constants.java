aefe88e937843326f63b88badd9ea6d793d93507
ArthurvanStrien <arthurvanstrien@gmail.com>
2016-12-18 16:21:27 +0100

    Added a class constants with the constants of the line detection.

---
+++ Constants.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Constants here.
+ * 
+ * @author  (Arthur van Strien) 
+ * @version (1)
+ * @date    ()
+ */
+public final class Constants
+{
+   //Contants for line detection:
+   public static final int LINE_TRIGGER = 500;
+   public static final int LINE_DETECTOR_LEFT = 0;
+   public static final int LINE_DETECTOR_MIDDLE = 1;
+   public static final int LINE_DETECTOR_RIGHT = 2;
+   
+   //
+}
